4682db34f89e11485e17db6451499c647d554493
Joe <joe@fluidnumerics.com>
2023-08-24 16:29:17 -0400

    update example wth colormap

--- examples/plotModel2d.py
+++ examples/plotModel2d.py
@@ -2,6 +2,7 @@ import numpy as np
 import pyvista as pv
 import self.model2d as model2d
 import inspect, os.path
+import matplotlib
 
 # Get full path to examples/
 # From https://stackoverflow.com/questions/2632199/how-do-i-get-the-path-of-the-current-executed-file-in-python
@@ -18,13 +19,17 @@ print("============================")
 print(f"Model2D PyVista data : \n {model.pvdata} \n")
 print("============================")
 
-# Use the "active_scalars_name" field to set which field is plotted.
-model.pvdata.point_data.active_scalars_name = "eta"
 
 print(model.pvdata.point_data)
 
+colormap = matplotlib.colormaps['Greys']
+
 # Plot the field in 2-d
 pl = pv.Plotter()
-pl.add_mesh(model.pvdata)
+pl.add_mesh(model.pvdata,
+            scalars="eta",
+            cmap=colormap)
 pl.camera_position = 'xy'
 pl.save_graphic('sample.eps')
+pl.show()
+
